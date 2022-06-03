a402e1d42a45df2675459a0de393f03876486010
sierrabakerr <102761255+sierrabakerr@users.noreply.github.com>
2022-06-03 12:15:22 -0700

    Add files via upload

---
+++ p2-random.js
@@ -0,0 +1,43 @@
+/*
+    CIT 281 Project 2
+    Name: Sierra Baker
+*/
+
+// Returns a random number of letters between min (inclusive) and max (exclusive)
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  
+  let result = "";
+  
+  let lengthOfOutputString = getRandomInteger(5, 26);
+  
+  for (let i = 0; i < lengthOfOutputString; i++) {
+    result += alphabet[getRandomInteger(0, alphabet.length)];
+  }
+    // Returns a single random number between min (inclusive) and max (exclusive)
+    function getRandomLetter() {
+      const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+      return alphabet[getRandomInteger(0, alphabet.length)];
+    }
+    
+    console.log(getRandomLetter());
+    
+    // Returns 10 to 20 random letters between the minLength and maxLength
+    function getRandomString(minLength, maxLength) {
+      let result = "";
+      let lengthOfOutputString = getRandomInteger(minLength, maxLength);
+    
+      for (let i = 0; i < lengthOfOutputString; i++) {
+        result += getRandomLetter();
+      }
+      return result;
+    }
+    
+    console.log(getRandomString(10, 20));
+
+    // Returns a sorted string of letters in ascending order
+    function getSortedString(string) {string = "this is the backwards alphabet zyxwutsrqponmlkjihgfedcba".split(""); return string.sort().join("").trim();} 
+  console.log(getSortedString());
